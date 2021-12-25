de285e975857d085b3a619ae5d9a9ce8c7132e77
mchang35 <mandy_chang@brown.edu>
2021-12-25 00:04:21 -0600

    grid for contact info

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    document.querySelector(".header").style.height = window.innerHeight + "px";
-})
+// var ready = (callback) => {
+//     if (document.readyState != "loading") callback();
+//     else document.addEventListener("DOMContentLoaded", callback);
+// }
+// ready(() => {
+//     document.querySelector(".header").style.height = window.innerHeight + "px";
+// })
 
 // defines where the navigation bar will be FIXED!
-var navbar_sticky_Y = window.scrollY + window.innerHeight;
+// var navbar_sticky_Y = window.scrollY + window.innerHeight;
+var navbar_sticky_Y = document.getElementById("navbar").scrollTop;
 
 // when users scroll on the window, make sure that the navigation is sticky
 window.onscroll = function() {sticky_nav()};
@@ -24,7 +25,11 @@ function sticky_nav() {
 }
 
 // define a function for when they click on each button in the navigation
-function top_of_page() {
+function home() {
     document.body.scrollTop = 0; // for Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function submit() {
+    console.log("We are trying to submit now.");
 }
